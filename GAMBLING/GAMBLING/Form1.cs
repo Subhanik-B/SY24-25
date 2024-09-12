2254a09f295166fbd60808a3506ebe69d762dc0c
shoob <buntubix@gmail.com>
2024-09-12 08:41:42 -0700

    Gambling Pt.1

---
+++ GAMBLING/GAMBLING/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GAMBLING
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random{ }; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                int a = random.Next(1, 4);
+                if(a == 2)
+                {
+                    textBox2.Text = "You win!";
+                } else if(a == 3)
+                {
+                    textBox2.Text = "You lose!";
+                }
+                else
+                {
+                    textBox2.Text = "You tied!";
+                }
+                radioButton1.Checked = false;
+            }
+            if (radioButton2.Checked == true)
+            {
+                int a = random.Next(1, 4);
+                if (a == 1)
+                {
+                    textBox2.Text = "You lose!";
+                }
+                else if (a == 3)
+                {
+                    textBox2.Text = "You win!";
+                }
+                else
+                {
+                    textBox2.Text = "You tied!";
+                }
+                radioButton2.Checked = false;
+            }
+            if (radioButton3.Checked == true)
+            {
+                int a = random.Next(1, 4);
+                if (a == 2)
+                {
+                    textBox2.Text = "You lose!";
+                }
+                else if (a == 1)
+                {
+                    textBox2.Text = "You win!";
+                }
+                else
+                {
+                    textBox2.Text = "You tied!";
+                }
+                radioButton1.Checked = false;
+            }
+        }
+    }
+}
