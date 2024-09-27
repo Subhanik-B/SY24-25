80e12ade1d51fec25314141d0350e96584686626
shoob <buntubix@gmail.com>
2024-09-27 09:04:03 -0700

    FISH

---
+++ bananers/bananers/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bananers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random fishy = new Random();
+        int fish;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fish = fishy.Next();
+            textBox1.Text = fish.ToString();
+        }
+    }
+}
