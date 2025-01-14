b3271226ec827f5a164b99c92e68517d17a563f9
shoob <buntubix@gmail.com>
2025-01-14 09:33:45 -0700

    eeeeee

--- New Years/New Years/Form1.Designer.cs
+++ New Years/New Years/Form1.Designer.cs
@@ -37,6 +37,24 @@
             this.Bit_6 = new System.Windows.Forms.TextBox();
             this.Bit_7 = new System.Windows.Forms.TextBox();
             this.Bit_8 = new System.Windows.Forms.TextBox();
+            this.Bit_8B = new System.Windows.Forms.TextBox();
+            this.Bit_7B = new System.Windows.Forms.TextBox();
+            this.Bit_6B = new System.Windows.Forms.TextBox();
+            this.Bit_5B = new System.Windows.Forms.TextBox();
+            this.Bit_4B = new System.Windows.Forms.TextBox();
+            this.Bit_3B = new System.Windows.Forms.TextBox();
+            this.Bit_2B = new System.Windows.Forms.TextBox();
+            this.Bit_1B = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Bit_8C = new System.Windows.Forms.TextBox();
+            this.Bit_7C = new System.Windows.Forms.TextBox();
+            this.Bit_6C = new System.Windows.Forms.TextBox();
+            this.Bit_5C = new System.Windows.Forms.TextBox();
+            this.Bit_4C = new System.Windows.Forms.TextBox();
+            this.Bit_3C = new System.Windows.Forms.TextBox();
+            this.Bit_2C = new System.Windows.Forms.TextBox();
+            this.Bit_1C = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Bit_1
@@ -86,7 +104,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(320, 237);
+            this.textBox1.Location = new System.Drawing.Point(317, 100);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(154, 22);
@@ -119,11 +137,189 @@
             this.Bit_8.TabIndex = 8;
             this.Bit_8.TextChanged += new System.EventHandler(this.Bit_8_TextChanged);
             // 
+            // Bit_8B
+            // 
+            this.Bit_8B.Location = new System.Drawing.Point(269, 212);
+            this.Bit_8B.MaxLength = 1;
+            this.Bit_8B.Name = "Bit_8B";
+            this.Bit_8B.Size = new System.Drawing.Size(26, 22);
+            this.Bit_8B.TabIndex = 16;
+            this.Bit_8B.TextChanged += new System.EventHandler(this.Bit_8B_TextChanged);
+            // 
+            // Bit_7B
+            // 
+            this.Bit_7B.Location = new System.Drawing.Point(301, 212);
+            this.Bit_7B.MaxLength = 1;
+            this.Bit_7B.Name = "Bit_7B";
+            this.Bit_7B.Size = new System.Drawing.Size(26, 22);
+            this.Bit_7B.TabIndex = 15;
+            this.Bit_7B.TextChanged += new System.EventHandler(this.Bit_8B_TextChanged);
+            // 
+            // Bit_6B
+            // 
+            this.Bit_6B.Location = new System.Drawing.Point(333, 212);
+            this.Bit_6B.MaxLength = 1;
+            this.Bit_6B.Name = "Bit_6B";
+            this.Bit_6B.Size = new System.Drawing.Size(26, 22);
+            this.Bit_6B.TabIndex = 14;
+            this.Bit_6B.TextChanged += new System.EventHandler(this.Bit_8B_TextChanged);
+            // 
+            // Bit_5B
+            // 
+            this.Bit_5B.Location = new System.Drawing.Point(365, 212);
+            this.Bit_5B.MaxLength = 1;
+            this.Bit_5B.Name = "Bit_5B";
+            this.Bit_5B.Size = new System.Drawing.Size(26, 22);
+            this.Bit_5B.TabIndex = 13;
+            this.Bit_5B.TextChanged += new System.EventHandler(this.Bit_8B_TextChanged);
+            // 
+            // Bit_4B
+            // 
+            this.Bit_4B.Location = new System.Drawing.Point(397, 212);
+            this.Bit_4B.MaxLength = 1;
+            this.Bit_4B.Name = "Bit_4B";
+            this.Bit_4B.Size = new System.Drawing.Size(26, 22);
+            this.Bit_4B.TabIndex = 12;
+            this.Bit_4B.TextChanged += new System.EventHandler(this.Bit_8B_TextChanged);
+            // 
+            // Bit_3B
+            // 
+            this.Bit_3B.Location = new System.Drawing.Point(429, 212);
+            this.Bit_3B.MaxLength = 1;
+            this.Bit_3B.Name = "Bit_3B";
+            this.Bit_3B.Size = new System.Drawing.Size(26, 22);
+            this.Bit_3B.TabIndex = 11;
+            this.Bit_3B.TextChanged += new System.EventHandler(this.Bit_8B_TextChanged);
+            // 
+            // Bit_2B
+            // 
+            this.Bit_2B.Location = new System.Drawing.Point(461, 212);
+            this.Bit_2B.MaxLength = 1;
+            this.Bit_2B.Name = "Bit_2B";
+            this.Bit_2B.Size = new System.Drawing.Size(26, 22);
+            this.Bit_2B.TabIndex = 10;
+            this.Bit_2B.TextChanged += new System.EventHandler(this.Bit_8B_TextChanged);
+            // 
+            // Bit_1B
+            // 
+            this.Bit_1B.Location = new System.Drawing.Point(493, 212);
+            this.Bit_1B.MaxLength = 1;
+            this.Bit_1B.Name = "Bit_1B";
+            this.Bit_1B.Size = new System.Drawing.Size(26, 22);
+            this.Bit_1B.TabIndex = 9;
+            this.Bit_1B.TextChanged += new System.EventHandler(this.Bit_8B_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(317, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(154, 22);
+            this.textBox2.TabIndex = 17;
+            // 
+            // Bit_8C
+            // 
+            this.Bit_8C.Location = new System.Drawing.Point(269, 240);
+            this.Bit_8C.MaxLength = 1;
+            this.Bit_8C.Name = "Bit_8C";
+            this.Bit_8C.Size = new System.Drawing.Size(26, 22);
+            this.Bit_8C.TabIndex = 25;
+            this.Bit_8C.TextChanged += new System.EventHandler(this.Bit_8C_TextChanged);
+            // 
+            // Bit_7C
+            // 
+            this.Bit_7C.Location = new System.Drawing.Point(301, 240);
+            this.Bit_7C.MaxLength = 1;
+            this.Bit_7C.Name = "Bit_7C";
+            this.Bit_7C.Size = new System.Drawing.Size(26, 22);
+            this.Bit_7C.TabIndex = 24;
+            this.Bit_7C.TextChanged += new System.EventHandler(this.Bit_8C_TextChanged);
+            // 
+            // Bit_6C
+            // 
+            this.Bit_6C.Location = new System.Drawing.Point(333, 240);
+            this.Bit_6C.MaxLength = 1;
+            this.Bit_6C.Name = "Bit_6C";
+            this.Bit_6C.Size = new System.Drawing.Size(26, 22);
+            this.Bit_6C.TabIndex = 23;
+            this.Bit_6C.TextChanged += new System.EventHandler(this.Bit_8C_TextChanged);
+            // 
+            // Bit_5C
+            // 
+            this.Bit_5C.Location = new System.Drawing.Point(365, 240);
+            this.Bit_5C.MaxLength = 1;
+            this.Bit_5C.Name = "Bit_5C";
+            this.Bit_5C.Size = new System.Drawing.Size(26, 22);
+            this.Bit_5C.TabIndex = 22;
+            this.Bit_5C.TextChanged += new System.EventHandler(this.Bit_8C_TextChanged);
+            // 
+            // Bit_4C
+            // 
+            this.Bit_4C.Location = new System.Drawing.Point(397, 240);
+            this.Bit_4C.MaxLength = 1;
+            this.Bit_4C.Name = "Bit_4C";
+            this.Bit_4C.Size = new System.Drawing.Size(26, 22);
+            this.Bit_4C.TabIndex = 21;
+            this.Bit_4C.TextChanged += new System.EventHandler(this.Bit_8C_TextChanged);
+            // 
+            // Bit_3C
+            // 
+            this.Bit_3C.Location = new System.Drawing.Point(429, 240);
+            this.Bit_3C.MaxLength = 1;
+            this.Bit_3C.Name = "Bit_3C";
+            this.Bit_3C.Size = new System.Drawing.Size(26, 22);
+            this.Bit_3C.TabIndex = 20;
+            this.Bit_3C.TextChanged += new System.EventHandler(this.Bit_8C_TextChanged);
+            // 
+            // Bit_2C
+            // 
+            this.Bit_2C.Location = new System.Drawing.Point(461, 240);
+            this.Bit_2C.MaxLength = 1;
+            this.Bit_2C.Name = "Bit_2C";
+            this.Bit_2C.Size = new System.Drawing.Size(26, 22);
+            this.Bit_2C.TabIndex = 19;
+            this.Bit_2C.TextChanged += new System.EventHandler(this.Bit_8C_TextChanged);
+            // 
+            // Bit_1C
+            // 
+            this.Bit_1C.Location = new System.Drawing.Point(493, 240);
+            this.Bit_1C.MaxLength = 1;
+            this.Bit_1C.Name = "Bit_1C";
+            this.Bit_1C.Size = new System.Drawing.Size(26, 22);
+            this.Bit_1C.TabIndex = 18;
+            this.Bit_1C.TextChanged += new System.EventHandler(this.Bit_8C_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(317, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(154, 22);
+            this.textBox3.TabIndex = 26;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Bit_8C);
+            this.Controls.Add(this.Bit_7C);
+            this.Controls.Add(this.Bit_6C);
+            this.Controls.Add(this.Bit_5C);
+            this.Controls.Add(this.Bit_4C);
+            this.Controls.Add(this.Bit_3C);
+            this.Controls.Add(this.Bit_2C);
+            this.Controls.Add(this.Bit_1C);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Bit_8B);
+            this.Controls.Add(this.Bit_7B);
+            this.Controls.Add(this.Bit_6B);
+            this.Controls.Add(this.Bit_5B);
+            this.Controls.Add(this.Bit_4B);
+            this.Controls.Add(this.Bit_3B);
+            this.Controls.Add(this.Bit_2B);
+            this.Controls.Add(this.Bit_1B);
             this.Controls.Add(this.Bit_8);
             this.Controls.Add(this.Bit_7);
             this.Controls.Add(this.Bit_6);
@@ -151,6 +347,24 @@
         private System.Windows.Forms.TextBox Bit_6;
         private System.Windows.Forms.TextBox Bit_7;
         private System.Windows.Forms.TextBox Bit_8;
+        private System.Windows.Forms.TextBox Bit_8B;
+        private System.Windows.Forms.TextBox Bit_7B;
+        private System.Windows.Forms.TextBox Bit_6B;
+        private System.Windows.Forms.TextBox Bit_5B;
+        private System.Windows.Forms.TextBox Bit_4B;
+        private System.Windows.Forms.TextBox Bit_3B;
+        private System.Windows.Forms.TextBox Bit_2B;
+        private System.Windows.Forms.TextBox Bit_1B;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Bit_8C;
+        private System.Windows.Forms.TextBox Bit_7C;
+        private System.Windows.Forms.TextBox Bit_6C;
+        private System.Windows.Forms.TextBox Bit_5C;
+        private System.Windows.Forms.TextBox Bit_4C;
+        private System.Windows.Forms.TextBox Bit_3C;
+        private System.Windows.Forms.TextBox Bit_2C;
+        private System.Windows.Forms.TextBox Bit_1C;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- New Years/New Years/Form1.cs
+++ New Years/New Years/Form1.cs
@@ -19,6 +19,8 @@ namespace New_Years
 
 
         int[] total = new int[8];
+        int[] totalB = new int[8];
+        int[] totalC = new int[8];
         bool bit1 = false;
         bool bit2 = false;
         bool bit3 = false;
@@ -27,6 +29,22 @@ namespace New_Years
         bool bit6 = false;
         bool bit7 = false;
         bool bit8 = false;
+        bool bit1B = false;
+        bool bit2B = false;
+        bool bit3B = false;
+        bool bit4B = false;
+        bool bit5B = false;
+        bool bit6B = false;
+        bool bit7B = false;
+        bool bit8B = false;
+        bool bit1C = false;
+        bool bit2C = false;
+        bool bit3C = false;
+        bool bit4C = false;
+        bool bit5C = false;
+        bool bit6C = false;
+        bool bit7C = false;
+        bool bit8C = false;
 
         private void Bit_8_TextChanged(object sender, EventArgs e)
         {
@@ -118,6 +136,190 @@ namespace New_Years
                b += total[i];
             }
             textBox1.Text = b.ToString();
+        }
+
+        private void Bit_8B_TextChanged(object sender, EventArgs e)
+        {
+            if (Bit_1B.Text.Equals("1") && !bit1B)
+            {
+                totalB[0] = 1;
+                bit1B = true;
+            }
+            if (Bit_2B.Text.Equals("1") && !bit2B)
+            {
+                totalB[1] = 2;
+                bit2B = true;
+            }
+            if (Bit_3B.Text.Equals("1") && !bit3B)
+            {
+                totalB[2] = 4;
+                bit3B = true;
+            }
+            if (Bit_4B.Text.Equals("1") && !bit4B)
+            {
+                totalB[3] = 8;
+                bit4B = true;
+            }
+            if (Bit_5B.Text.Equals("1") && !bit5B)
+            {
+                totalB[4] = 16;
+                bit5B = true;
+            }
+            if (Bit_6B.Text.Equals("1") && !bit6B)
+            {
+                totalB[5] = 32;
+                bit6B = true;
+            }
+            if (Bit_7B.Text.Equals("1") && !bit7B)
+            {
+                totalB[6] = 64;
+                bit7B = true;
+            }
+            if (Bit_8B.Text.Equals("1") && !bit8B)
+            {
+                totalB[7] = 128;
+                bit8B = true;
+            }
+
+
+            if (Bit_1B.Text.Equals("0") && bit1B)
+            {
+                totalB[0] = 0;
+                bit1B = false;
+            }
+            if (Bit_2B.Text.Equals("0") && bit2B)
+            {
+                totalB[1] = 0;
+                bit2B = false;
+            }
+            if (Bit_3B.Text.Equals("0") && bit3B)
+            {
+                totalB[2] = 0;
+                bit3B = false;
+            }
+            if (Bit_4B.Text.Equals("0") && bit4B)
+            {
+                totalB[3] = 0;
+                bit4B = false;
+            }
+            if (Bit_5B.Text.Equals("0") && bit5B)
+            {
+                totalB[4] = 0;
+                bit5B = false;
+            }
+            if (Bit_6B.Text.Equals("0") && bit6B)
+            {
+                totalB[5] = 0;
+                bit6B = false;
+            }
+            if (Bit_7B.Text.Equals("0") && bit7B)
+            {
+                totalB[6] = 0;
+                bit7B = false;
+            }
+            if (Bit_8B.Text.Equals("0") && bit8B)
+            {
+                totalB[7] = 0;
+                bit8B = false;
+            }
+            int b = 0;
+            for (int i = 0; i < totalB.Length; i++)
+            {
+                b += totalB[i];
+            }
+            textBox2.Text = b.ToString();
+        }
+
+        private void Bit_8C_TextChanged(object sender, EventArgs e)
+        {
+            if (Bit_1C.Text.Equals("1") && !bit1C)
+            {
+                totalC[0] = 1;
+                bit1C = true;
+            }
+            if (Bit_2C.Text.Equals("1") && !bit2C)
+            {
+                totalC[1] = 2;
+                bit2C = true;
+            }
+            if (Bit_3C.Text.Equals("1") && !bit3C)
+            {
+                totalC[2] = 4;
+                bit3C = true;
+            }
+            if (Bit_4C.Text.Equals("1") && !bit4C)
+            {
+                totalC[3] = 8;
+                bit4C = true;
+            }
+            if (Bit_5C.Text.Equals("1") && !bit5C)
+            {
+                totalC[4] = 16;
+                bit5C = true;
+            }
+            if (Bit_6C.Text.Equals("1") && !bit6C)
+            {
+                totalC[5] = 32;
+                bit6C = true;
+            }
+            if (Bit_7C.Text.Equals("1") && !bit7C)
+            {
+                totalC[6] = 64;
+                bit7C = true;
+            }
+            if (Bit_8C.Text.Equals("1") && !bit8C)
+            {
+                totalC[7] = 128;
+                bit8C = true;
+            }
+
+
+            if (Bit_1C.Text.Equals("0") && bit1C)
+            {
+                totalC[0] = 0;
+                bit1C = false;
+            }
+            if (Bit_2C.Text.Equals("0") && bit2C)
+            {
+                totalC[1] = 0;
+                bit2C = false;
+            }
+            if (Bit_3C.Text.Equals("0") && bit3C)
+            {
+                totalC[2] = 0;
+                bit3C = false;
+            }
+            if (Bit_4C.Text.Equals("0") && bit4C)
+            {
+                totalC[3] = 0;
+                bit4C = false;
+            }
+            if (Bit_5C.Text.Equals("0") && bit5C)
+            {
+                totalC[4] = 0;
+                bit5C = false;
+            }
+            if (Bit_6C.Text.Equals("0") && bit6C)
+            {
+                totalC[5] = 0;
+                bit6C = false;
+            }
+            if (Bit_7C.Text.Equals("0") && bit7C)
+            {
+                totalC[6] = 0;
+                bit7C = false;
+            }
+            if (Bit_8C.Text.Equals("0") && bit8C)
+            {
+                totalC[7] = 0;
+                bit8C = false;
+            }
+            int b = 0;
+            for (int i = 0; i < totalC.Length; i++)
+            {
+                b += totalC[i];
+            }
+            textBox3.Text = b.ToString();
         }
     }
 }
