fb6695f3b4c4e5aa2cfbb8056b8e3936eea64db2
shoob <buntubix@gmail.com>
2025-02-14 08:32:27 -0700

    htr

---
+++ Morebeans/Morebeans/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morebeans
+{
+    internal class Class1
+    {
+        public int maxSpeed { get; set; }
+        public double toHundred { get; set; }
+        public int cylinders { get; set; }
+        public int rpm { get; set; }
+        public int horsepower { get; set; }
+        public int sizeOfCylinders { get; set; }
+    }
+}
---
+++ Morebeans/Morebeans/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morebeans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static Class1 car(int speed, double toH, int cylinders, int rpm, int horsepower, int cylinderSize)
+        {
+            Class1 b = new Class1();
+            b.rpm = rpm;
+            b.maxSpeed = speed;
+            b.sizeOfCylinders = cylinderSize;
+            b.cylinders = cylinders;
+            b.toHundred = toH;
+
+        }
+    }
+}
