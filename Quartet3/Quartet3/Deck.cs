ee4c0ddc7c0b25d03f9e806bd53b1f6c3eef630a
shoob <buntubix@gmail.com>
2025-02-24 08:47:18 -0700

    k

---
+++ Quartet3/Quartet3/Deck.cs
@@ -0,0 +1,54 @@
+﻿using Quartet3;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartet3
+{
+    internal class Deck
+    {
+        List<CarCard> carCards;
+        Random random = new Random();
+        public Deck(List<CarCard> cards)
+        {
+            this.carCards = cards;
+        }
+        public CarCard GetCard(int index)
+        {
+            CarCard C = carCards[index];
+            carCards.RemoveAt(index);
+            return C;
+        }
+        public override string ToString()
+        {
+            string retVal = "Deck:\n";
+            foreach (CarCard card in carCards)
+            {
+                retVal += card.ToString();
+            }
+            return retVal;
+        }
+        public void updateDeck(List<CarCard> cards)
+        {
+            this.carCards = cards;
+        }
+
+        public void shuffle()
+        {
+            int n = 0;
+            int n2 = 0;
+            n = random.Next(carCards.Count);
+            CarCard temp;
+            for (int i = 0; i < carCards.Count; i++)
+            {
+                n2 = random.Next(carCards.Count);
+                temp = carCards[n];
+                carCards[n] = carCards[n2];
+                carCards[n2] = temp;
+            }
+        }
+    }
+}
---
+++ Quartet3/Quartet3/Hand.cs
@@ -0,0 +1,19 @@
+﻿using Quartet3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartet3
+{
+    internal class Hand
+    {
+        List<CarCard> carCards;
+
+        public CarCard getTop()
+        {
+            return carCards[0];
+        }
+    }
+}
