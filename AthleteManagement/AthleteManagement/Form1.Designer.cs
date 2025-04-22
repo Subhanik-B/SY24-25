d982f26a3c3d4aa5a414925d1d5a8711273ffd1d
Subhanik <buntubix@gmail.com>
2025-04-21 17:23:26 -0700

    juym

--- AthleteManagement/AthleteManagement/Form1.Designer.cs
+++ AthleteManagement/AthleteManagement/Form1.Designer.cs
@@ -46,24 +46,28 @@
             this.AddAtButton = new System.Windows.Forms.Button();
             this.NextBttn = new System.Windows.Forms.Button();
             this.PreviousBttn = new System.Windows.Forms.Button();
+            this.FinishEdit = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(202, 113);
+            this.dataGridView1.Location = new System.Drawing.Point(26, 92);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(376, 254);
+            this.dataGridView1.Size = new System.Drawing.Size(552, 206);
             this.dataGridView1.TabIndex = 0;
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(250, 84);
+            this.AddButton.Location = new System.Drawing.Point(188, 68);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.Size = new System.Drawing.Size(56, 19);
             this.AddButton.TabIndex = 1;
             this.AddButton.Text = "Add";
             this.AddButton.UseVisualStyleBackColor = true;
@@ -71,9 +75,10 @@
             // 
             // EditButton
             // 
-            this.EditButton.Location = new System.Drawing.Point(343, 84);
+            this.EditButton.Location = new System.Drawing.Point(257, 68);
+            this.EditButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.EditButton.Name = "EditButton";
-            this.EditButton.Size = new System.Drawing.Size(75, 23);
+            this.EditButton.Size = new System.Drawing.Size(56, 19);
             this.EditButton.TabIndex = 2;
             this.EditButton.Text = "Edit";
             this.EditButton.UseVisualStyleBackColor = true;
@@ -81,58 +86,66 @@
             // 
             // DeleteButton
             // 
-            this.DeleteButton.Location = new System.Drawing.Point(438, 84);
+            this.DeleteButton.Location = new System.Drawing.Point(328, 68);
+            this.DeleteButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.Size = new System.Drawing.Size(56, 19);
             this.DeleteButton.TabIndex = 3;
             this.DeleteButton.Text = "Delete";
             this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(305, 56);
+            this.textBox1.Location = new System.Drawing.Point(229, 46);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(155, 22);
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(96, 84);
+            this.textBox2.Location = new System.Drawing.Point(72, 68);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
             this.textBox2.TabIndex = 5;
             this.textBox2.Visible = false;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(96, 113);
+            this.textBox3.Location = new System.Drawing.Point(72, 92);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
             this.textBox3.TabIndex = 6;
             this.textBox3.Visible = false;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(96, 141);
+            this.textBox4.Location = new System.Drawing.Point(72, 115);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.Size = new System.Drawing.Size(76, 20);
             this.textBox4.TabIndex = 7;
             this.textBox4.Visible = false;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(96, 169);
+            this.textBox5.Location = new System.Drawing.Point(72, 137);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.Size = new System.Drawing.Size(76, 20);
             this.textBox5.TabIndex = 8;
             this.textBox5.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 84);
+            this.label1.Location = new System.Drawing.Point(10, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.Size = new System.Drawing.Size(60, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "First Name:";
             this.label1.Visible = false;
@@ -140,9 +153,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 113);
+            this.label2.Location = new System.Drawing.Point(9, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 10;
             this.label2.Text = "Last Name:";
             this.label2.Visible = false;
@@ -150,9 +164,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 144);
+            this.label3.Location = new System.Drawing.Point(10, 117);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(23, 16);
+            this.label3.Size = new System.Drawing.Size(21, 13);
             this.label3.TabIndex = 11;
             this.label3.Text = "ID:";
             this.label3.Visible = false;
@@ -160,9 +175,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 175);
+            this.label4.Location = new System.Drawing.Point(9, 142);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.Size = new System.Drawing.Size(26, 13);
             this.label4.TabIndex = 12;
             this.label4.Text = "Age";
             this.label4.Visible = false;
@@ -170,26 +186,29 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 203);
+            this.label5.Location = new System.Drawing.Point(9, 165);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 16);
+            this.label5.Size = new System.Drawing.Size(65, 13);
             this.label5.TabIndex = 14;
             this.label5.Text = "Bib Number:";
             this.label5.Visible = false;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(96, 197);
+            this.textBox6.Location = new System.Drawing.Point(72, 160);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.Size = new System.Drawing.Size(76, 20);
             this.textBox6.TabIndex = 13;
             this.textBox6.Visible = false;
             // 
             // AddAtButton
             // 
-            this.AddAtButton.Location = new System.Drawing.Point(54, 234);
+            this.AddAtButton.Location = new System.Drawing.Point(40, 190);
+            this.AddAtButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AddAtButton.Name = "AddAtButton";
-            this.AddAtButton.Size = new System.Drawing.Size(75, 23);
+            this.AddAtButton.Size = new System.Drawing.Size(56, 19);
             this.AddAtButton.TabIndex = 15;
             this.AddAtButton.Text = "Add";
             this.AddAtButton.UseVisualStyleBackColor = true;
@@ -198,29 +217,59 @@
             // 
             // NextBttn
             // 
-            this.NextBttn.Location = new System.Drawing.Point(121, 234);
+            this.NextBttn.Location = new System.Drawing.Point(91, 190);
+            this.NextBttn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.NextBttn.Name = "NextBttn";
-            this.NextBttn.Size = new System.Drawing.Size(75, 23);
+            this.NextBttn.Size = new System.Drawing.Size(56, 19);
             this.NextBttn.TabIndex = 16;
             this.NextBttn.Text = "Next ->";
             this.NextBttn.UseVisualStyleBackColor = true;
             this.NextBttn.Visible = false;
+            this.NextBttn.Click += new System.EventHandler(this.NextBttn_Click);
             // 
             // PreviousBttn
             // 
-            this.PreviousBttn.Location = new System.Drawing.Point(3, 234);
+            this.PreviousBttn.Location = new System.Drawing.Point(2, 190);
+            this.PreviousBttn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PreviousBttn.Name = "PreviousBttn";
-            this.PreviousBttn.Size = new System.Drawing.Size(75, 23);
+            this.PreviousBttn.Size = new System.Drawing.Size(56, 19);
             this.PreviousBttn.TabIndex = 17;
             this.PreviousBttn.Text = "<- Next";
             this.PreviousBttn.UseVisualStyleBackColor = true;
             this.PreviousBttn.Visible = false;
+            this.PreviousBttn.Click += new System.EventHandler(this.PreviousBttn_Click);
+            // 
+            // FinishEdit
+            // 
+            this.FinishEdit.Location = new System.Drawing.Point(40, 213);
+            this.FinishEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.FinishEdit.Name = "FinishEdit";
+            this.FinishEdit.Size = new System.Drawing.Size(56, 19);
+            this.FinishEdit.TabIndex = 18;
+            this.FinishEdit.Text = "Done";
+            this.FinishEdit.UseVisualStyleBackColor = true;
+            this.FinishEdit.Visible = false;
+            this.FinishEdit.Click += new System.EventHandler(this.FinishEdit_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(40, 213);
+            this.Delete.Margin = new System.Windows.Forms.Padding(2);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(56, 19);
+            this.Delete.TabIndex = 19;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Visible = false;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.FinishEdit);
             this.Controls.Add(this.PreviousBttn);
             this.Controls.Add(this.NextBttn);
             this.Controls.Add(this.AddAtButton);
@@ -239,6 +288,7 @@
             this.Controls.Add(this.EditButton);
             this.Controls.Add(this.AddButton);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -268,6 +318,8 @@
         private System.Windows.Forms.Button AddAtButton;
         private System.Windows.Forms.Button NextBttn;
         private System.Windows.Forms.Button PreviousBttn;
+        private System.Windows.Forms.Button FinishEdit;
+        private System.Windows.Forms.Button Delete;
     }
 }
 
--- AthleteManagement/AthleteManagement/Form1.cs
+++ AthleteManagement/AthleteManagement/Form1.cs
@@ -15,9 +15,11 @@ namespace AthleteManagement
 {
     public partial class Form1 : Form
     {
-        DatabaseHelper databaseHelper = new DatabaseHelper("C:\\Users\\sbha527\\Documents\\ah\\SY24-25\\AthleteManagement\\AthleteManagement\\bin\\Debug\\Athletes.xml");
+        //DatabaseHelper databaseHelper = new DatabaseHelper("C:\\Users\\sbha527\\Documents\\ah\\SY24-25\\AthleteManagement\\AthleteManagement\\bin\\Debug\\Athletes.xml");
+        DatabaseHelper databaseHelper = new DatabaseHelper("C:\\Users\\buntu\\OneDrive\\Documents\\School Code\\SY24-25\\AthleteManagement\\AthleteManagement\\bin\\Debug\\DebugAthletes.xml");
         int globalID = 1;
         List<Athlete> athletes = new List<Athlete> {};
+        int editTracker = 0;
         public Form1()
         {
             InitializeComponent();
@@ -25,18 +27,36 @@ namespace AthleteManagement
         private void Form1_Load(object sender, EventArgs e)
         {
             dataGridView1.Columns.Add("Athlete Name", "Athlete Name");
+            dataGridView1.Columns.Add("Team", "Team");
             dataGridView1.Columns.Add("ID", "ID");
             dataGridView1.Columns.Add("Age", "Age");
+            dataGridView1.Columns.Add("Bib Number", "Bib Number");
+            
             athletes = databaseHelper.GetAllAthletes();
             try
             {
-                dataGridView1.Rows.Add(athletes.Count() - 1);
-                for (int i = 0; i < athletes.Count(); i++)
+                if (athletes.Count() == 1)
                 {
-                    dataGridView1.Rows[i].Cells[0].Value = athletes[i].FullName;
-                    dataGridView1.Rows[i].Cells[1].Value = athletes[i].AthleteID;
-                    dataGridView1.Rows[i].Cells[2].Value = athletes[i].Age;
-                    globalID++;
+                    for (int i = 0; i < athletes.Count(); i++)
+                    {
+                        dataGridView1.Rows[i].Cells[0].Value = athletes[i].FullName;
+                        dataGridView1.Rows[i].Cells[2].Value = athletes[i].AthleteID;
+                        dataGridView1.Rows[i].Cells[3].Value = athletes[i].Age;
+                        dataGridView1.Rows[i].Cells[4].Value = athletes[i].BibNumber;
+                        globalID++;
+                    }
+                }
+                else
+                {
+                    dataGridView1.Rows.Add(athletes.Count() - 1);
+                    for (int i = 0; i < athletes.Count(); i++)
+                    {
+                        dataGridView1.Rows[i].Cells[0].Value = athletes[i].FullName;
+                        dataGridView1.Rows[i].Cells[2].Value = athletes[i].AthleteID;
+                        dataGridView1.Rows[i].Cells[3].Value = athletes[i].Age;
+                        dataGridView1.Rows[i].Cells[4].Value = athletes[i].BibNumber;
+                        globalID++;
+                    }
                 }
             }
             catch (Exception ex)
@@ -101,10 +121,18 @@ namespace AthleteManagement
             for (int i = 0; i < athletes.Count(); i++)
             {
                 dataGridView1.Rows[i].Cells[0].Value = athletes[i].FullName;
-                dataGridView1.Rows[i].Cells[1].Value = athletes[i].AthleteID;
-                dataGridView1.Rows[i].Cells[2].Value = athletes[i].Age;
+                dataGridView1.Rows[i].Cells[2].Value = athletes[i].AthleteID;
+                dataGridView1.Rows[i].Cells[3].Value = athletes[i].Age;
+                dataGridView1.Rows[i].Cells[4].Value = athletes[i].BibNumber;
             }
             globalID++;
+
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+
         }
 
         private void EditButton_Click(object sender, EventArgs e)
@@ -126,6 +154,208 @@ namespace AthleteManagement
             label5.Visible = true;
             NextBttn.Visible = true;
             PreviousBttn.Visible = true;
+            FinishEdit.Visible = true;
+
+            textBox2.Text = athletes[editTracker].FirstName;
+            textBox3.Text = athletes[editTracker].LastName;
+            textBox4.Text = athletes[editTracker].AthleteID.ToString();
+            textBox5.Text = athletes[editTracker].Age.ToString();
+            textBox6.Text = athletes[editTracker].BibNumber;
+        }
+
+        private void NextBttn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Athlete uAthlete = new Athlete();
+                uAthlete.FirstName = textBox2.Text;
+                uAthlete.LastName = textBox3.Text;
+                uAthlete.AthleteID = int.Parse(textBox4.Text);
+                uAthlete.Age = int.Parse(textBox5.Text);
+                uAthlete.BibNumber = textBox6.Text;
+
+                databaseHelper.UpdateAthlete(uAthlete);
+
+                editTracker++;
+                textBox2.Text = athletes[editTracker].FirstName;
+                textBox3.Text = athletes[editTracker].LastName;
+                textBox4.Text = athletes[editTracker].AthleteID.ToString();
+                textBox5.Text = athletes[editTracker].Age.ToString();
+                textBox6.Text = athletes[editTracker].BibNumber;
+            }
+            catch (Exception ex)
+            {
+                Athlete uAthlete = new Athlete();
+                uAthlete.FirstName = textBox2.Text;
+                uAthlete.LastName = textBox3.Text;
+                uAthlete.AthleteID = int.Parse(textBox4.Text);
+                uAthlete.Age = int.Parse(textBox5.Text);
+                uAthlete.BibNumber = textBox6.Text;
+
+                databaseHelper.UpdateAthlete(uAthlete);
+
+                editTracker = 0;
+                textBox2.Text = athletes[editTracker].FirstName;
+                textBox3.Text = athletes[editTracker].LastName;
+                textBox4.Text = athletes[editTracker].AthleteID.ToString();
+                textBox5.Text = athletes[editTracker].Age.ToString();
+                textBox6.Text = athletes[editTracker].BibNumber;
+            }
+        }
+
+        private void PreviousBttn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Athlete uAthlete = new Athlete();
+                uAthlete.FirstName = textBox2.Text;
+                uAthlete.LastName = textBox3.Text;
+                uAthlete.AthleteID = int.Parse(textBox4.Text);
+                uAthlete.Age = int.Parse(textBox5.Text);
+                uAthlete.BibNumber = textBox6.Text;
+
+                databaseHelper.UpdateAthlete(uAthlete);
+
+                editTracker--;
+                textBox2.Text = athletes[editTracker].FirstName;
+                textBox3.Text = athletes[editTracker].LastName;
+                textBox4.Text = athletes[editTracker].AthleteID.ToString();
+                textBox5.Text = athletes[editTracker].Age.ToString();
+                textBox6.Text = athletes[editTracker].BibNumber;
+            }
+            catch (Exception ex)
+            {
+                Athlete uAthlete = new Athlete();
+                uAthlete.FirstName = textBox2.Text;
+                uAthlete.LastName = textBox3.Text;
+                uAthlete.AthleteID = int.Parse(textBox4.Text);
+                uAthlete.Age = int.Parse(textBox5.Text);
+                uAthlete.BibNumber = textBox6.Text;
+
+                databaseHelper.UpdateAthlete(uAthlete);
+
+                editTracker = 0;
+                textBox2.Text = athletes[editTracker].FirstName;
+                textBox3.Text = athletes[editTracker].LastName;
+                textBox4.Text = athletes[editTracker].AthleteID.ToString();
+                textBox5.Text = athletes[editTracker].Age.ToString();
+                textBox6.Text = athletes[editTracker].BibNumber;
+            }
+        }
+
+        private void FinishEdit_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            AddButton.Visible = true;
+            EditButton.Visible = true;
+            DeleteButton.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            textBox5.Visible = false;
+            textBox6.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            AddAtButton.Visible = false;
+            NextBttn.Visible = false;
+            PreviousBttn.Visible = false;
+            FinishEdit.Visible = false;
+
+            Athlete uAthlete = new Athlete();
+            uAthlete.FirstName = textBox2.Text;
+            uAthlete.LastName = textBox3.Text;
+            uAthlete.AthleteID = int.Parse(textBox4.Text);
+            uAthlete.Age = int.Parse(textBox5.Text);
+            uAthlete.BibNumber = textBox6.Text;
+
+            databaseHelper.UpdateAthlete(uAthlete);
+
+            athletes = databaseHelper.GetAllAthletes();
+            for (int i = 0; i < athletes.Count(); i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = athletes[i].FullName;
+                dataGridView1.Rows[i].Cells[2].Value = athletes[i].AthleteID;
+                dataGridView1.Rows[i].Cells[3].Value = athletes[i].Age;
+                dataGridView1.Rows[i].Cells[4].Value = athletes[i].BibNumber;
+            }
+
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = false;
+            AddButton.Visible = false;
+            EditButton.Visible = false;
+            DeleteButton.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            textBox5.Visible = true;
+            textBox6.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            NextBttn.Visible = true;
+            PreviousBttn.Visible = true;
+            Delete.Visible = true;
+
+            textBox2.Text = athletes[editTracker].FirstName;
+            textBox3.Text = athletes[editTracker].LastName;
+            textBox4.Text = athletes[editTracker].AthleteID.ToString();
+            textBox5.Text = athletes[editTracker].Age.ToString();
+            textBox6.Text = athletes[editTracker].BibNumber;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible = true;
+            AddButton.Visible = true;
+            EditButton.Visible = true;
+            DeleteButton.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            textBox5.Visible = false;
+            textBox6.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            AddAtButton.Visible = false;
+            NextBttn.Visible = false;
+            PreviousBttn.Visible = false;
+            Delete.Visible = false;
+
+            databaseHelper.DeleteAthlete(athletes[editTracker].AthleteID);
+            dataGridView1.Rows.Clear();
+            athletes = databaseHelper.GetAllAthletes();
+            for (int i = 0; i < athletes.Count(); i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = athletes[i].FullName;
+                dataGridView1.Rows[i].Cells[2].Value = athletes[i].AthleteID;
+                dataGridView1.Rows[i].Cells[3].Value = athletes[i].Age;
+                dataGridView1.Rows[i].Cells[4].Value = athletes[i].BibNumber;
+            }
+
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
         }
     }
 }
